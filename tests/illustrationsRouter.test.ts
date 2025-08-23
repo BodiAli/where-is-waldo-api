852b2c3ccce5b40a35928cd21bdea0f9f1744f40
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-23 22:00:36 +0300

    Feat: create tests for /:illustrationId route

--- tests/illustrationsRouter.test.ts
+++ tests/illustrationsRouter.test.ts
@@ -1,30 +1,63 @@
 import express from "express";
 import request from "supertest";
-import type { Illustration } from "@prisma/client";
+import type { Character, Illustration } from "@prisma/client";
 import illustrationsRouter from "../routes/illustrationsRouter.js";
+import prisma from "../prisma/prismaClient.js";
 
 const app = express();
 
 app.use("/illustrations", illustrationsRouter);
-
-interface IllustrationsResponse {
-  illustrations: Illustration[];
-}
 
 describe("illustrationsRouter routes", () => {
   describe("given GET request to /illustrations", () => {
     it("should return 200 status and all illustrations", async () => {
       const response = await request(app).get("/illustrations").expect("Content-type", /json/).expect(200);
 
-      const responseBody = response.body as IllustrationsResponse;
-
-      console.log(responseBody.illustrations);
+      const responseBody = response.body as { illustrations: Illustration[] };
 
       expect(responseBody.illustrations).toMatchObject([
         { difficulty: "easy" },
         { difficulty: "medium" },
         { difficulty: "hard" },
       ]);
+    });
+  });
+
+  describe("given GET request to /illustrations/:illustrationId", async () => {
+    const mediumIllustration = await prisma.illustration.findUnique({
+      where: {
+        difficulty: "medium",
+      },
+    });
+
+    if (!mediumIllustration) {
+      throw new Error("Medium illustration not found");
+    }
+
+    it("should return 200 status with the requested illustration and corresponding characters", async () => {
+      const response = await request(app)
+        .get(`/illustrations/${mediumIllustration.id}`)
+        .expect("Content-type", /json/)
+        .expect(200);
+
+      const responseBody = response.body as { illustration: Illustration & { Characters: Character[] } };
+      expect(responseBody.illustration).toMatchObject({ id: mediumIllustration.id, difficulty: "medium" });
+      expect(responseBody.illustration.Characters).toMatchObject([
+        { name: "waldo" },
+        { name: "wenda" },
+        { name: "wizard" },
+      ]);
+    });
+
+    it("should return 404 status with error message if illustration is not found", async () => {
+      const response = await request(app)
+        .get("/illustrations/invalidId")
+        .expect("Content-type", /json/)
+        .expect(404);
+
+      const responseBody = response.body as { error: string };
+
+      expect(responseBody.error).toBe("Illustration not found");
     });
   });
 });
