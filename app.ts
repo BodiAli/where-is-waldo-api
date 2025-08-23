09cbf476d545755d41f5e228d63973e85bfe461c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-23 18:23:34 +0300

    Feat: Initialize app

---
+++ app.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express, { type Request, type Response, type NextFunction } from "express";
+import illustrationsRouter from "./routes/illustrationsRouter.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/illustrations", illustrationsRouter);
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({ error: "Resource not found" });
+});
+
+app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(error);
+  res.status(500).json({ error: error.message ? error.message : error });
+});
+
+const port = process.env?.["PORT"] ?? 3000;
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}!`);
+});
