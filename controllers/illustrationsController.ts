c675a29808750657f1ba93adf37e2382ebe1a62d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-23 19:43:57 +0300

    Feat: Send illustration

--- controllers/illustrationsController.ts
+++ controllers/illustrationsController.ts
@@ -1,5 +1,8 @@
 import type { Request, Response } from "express";
+import prisma from "../prisma/prismaClient.js";
 
-export function getIllustrations(_req: Request, res: Response) {
-  res.json({ msg: "hii" });
+export async function getIllustrations(_req: Request, res: Response) {
+  const illustrations = await prisma.illustration.findMany();
+
+  res.json({ illustrations });
 }
