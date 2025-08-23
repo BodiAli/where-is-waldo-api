ccbc347ced0b122016157be97a371513a33a3201
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-23 22:01:03 +0300

    Feat: Create controller for getting a single illustration

--- controllers/illustrationsController.ts
+++ controllers/illustrationsController.ts
@@ -6,3 +6,23 @@ export async function getIllustrations(_req: Request, res: Response) {
 
   res.json({ illustrations });
 }
+
+export async function getIllustration(req: Request<{ illustrationId: string }>, res: Response) {
+  const { illustrationId } = req.params;
+
+  const illustration = await prisma.illustration.findUnique({
+    where: {
+      id: illustrationId,
+    },
+    include: {
+      Characters: true,
+    },
+  });
+
+  if (!illustration) {
+    res.status(404).json({ error: "Illustration not found" });
+    return;
+  }
+
+  res.json({ illustration });
+}
