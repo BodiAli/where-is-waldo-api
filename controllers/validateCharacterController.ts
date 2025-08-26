e3949d36d42d2acf1e0bc285ebd07ea6f8436531
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-27 02:39:31 +0300

    Feat: Create a function to check if all characters are found && refactor middlewares to their own function declarations

--- controllers/validateCharacterController.ts
+++ controllers/validateCharacterController.ts
@@ -1,45 +1,91 @@
 import type { NextFunction, Request, Response } from "express";
 import prisma from "../prisma/prismaClient.js";
 
-export const validate = [
-  async (req: Request<{ characterId: string }>, res: Response, next: NextFunction) => {
-    const { characterId } = req.params;
-
-    const character = await prisma.character.findUnique({
+async function areAllCharactersFound(illustrationId: string) {
+  const [total, found] = await Promise.all([
+    prisma.character.count({
       where: {
-        id: characterId,
+        illustrationId,
       },
-    });
+    }),
+    prisma.character.count({
+      where: {
+        illustrationId,
+        isFound: true,
+      },
+    }),
+  ]);
 
-    if (!character) {
-      res.status(404).json({ error: "Character not found" });
-      return;
-    }
+  return total === found;
+}
 
-    req.character = character;
+async function getCharacterAndPopulateRequest(
+  req: Request<{ characterId: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  const { characterId } = req.params;
 
-    next();
-  },
+  const character = await prisma.character.findUnique({
+    where: {
+      id: characterId,
+    },
+  });
 
-  (req: Request<object, object, { xPosition: string; yPosition: string }>, res: Response) => {
-    const xPosition = Number.parseInt(req.body.xPosition);
-    const yPosition = Number.parseInt(req.body.yPosition);
-
-    const character = req.character;
-
-    if (
-      !(
-        xPosition >= character.xStart &&
-        xPosition <= character.xEnd &&
-        yPosition >= character.yStart &&
-        yPosition <= character.yEnd
-      )
-    ) {
-      res.sendStatus(400);
-      return;
-    }
-
-    res.status(200).json({ character });
+  if (!character) {
+    res.status(404).json({ error: "Character not found" });
     return;
-  },
-];
+  }
+
+  req.character = character;
+
+  next();
+}
+
+async function validateCharacterPosition(
+  req: Request<object, object, { xPosition: string; yPosition: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  const xPosition = Number.parseInt(req.body.xPosition);
+  const yPosition = Number.parseInt(req.body.yPosition);
+
+  const character = req.character;
+
+  if (
+    !(
+      xPosition >= character.xStart &&
+      xPosition <= character.xEnd &&
+      yPosition >= character.yStart &&
+      yPosition <= character.yEnd
+    )
+  ) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const updatedCharacter = await prisma.character.update({
+    data: {
+      isFound: true,
+    },
+    where: {
+      id: character.id,
+    },
+  });
+
+  if (await areAllCharactersFound(character.illustrationId)) {
+    next();
+    return;
+  }
+
+  res.status(200).json({ character: updatedCharacter, msg: `You found ${updatedCharacter.name}` });
+  return;
+}
+
+function handleGameWon(req: Request, res: Response) {
+  res.status(200).json({
+    msg: "Game won",
+  });
+}
+
+export const validate = [getCharacterAndPopulateRequest, validateCharacterPosition, handleGameWon];
