9e0b02883a0d3dd53a6200aef4a3826cb7d80eb7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-26 20:14:41 +0300

    Feat: Use validateCharacterRouter for the /validate route

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express, { type Request, type Response, type NextFunction } from "express";
 import illustrationsRouter from "./routes/illustrationsRouter.js";
+import validateCharacterRouter from "./routes/validateCharacterRouter.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/illustrations", illustrationsRouter);
+app.use("/validate", validateCharacterRouter);
 
 app.use((_req: Request, res: Response) => {
   res.status(404).json({ error: "Resource not found" });
