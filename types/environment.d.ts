5dbb8a8a533bc1d9a2e9db5bf5f908cb59f6326a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-26 20:37:56 +0300

    Feat: Augment process.env object with environment variables

---
+++ types/environment.d.ts
@@ -0,0 +1,16 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    WALDO_MEDIUM_X_START: string;
+    WALDO_MEDIUM_X_END: string;
+    WALDO_MEDIUM_Y_START: string;
+    WALDO_MEDIUM_Y_END: string;
+    WENDA_MEDIUM_X_START: string;
+    WENDA_MEDIUM_X_END: string;
+    WENDA_MEDIUM_Y_START: string;
+    WENDA_MEDIUM_Y_END: string;
+    WIZARD_MEDIUM_X_START: string;
+    WIZARD_MEDIUM_X_END: string;
+    WIZARD_MEDIUM_Y_START: string;
+    WIZARD_MEDIUM_Y_END: string;
+  }
+}
